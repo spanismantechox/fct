ddcba2517c4add91c01ec0a0d6e2726b394ad5e9
spanismantechox <56296709+spanismantechox@users.noreply.github.com>
2019-12-05 16:26:11 +0100

    src

--- src/main/java/com/app/fct/repositories/GastoRepository.java
+++ src/main/java/com/app/fct/repositories/GastoRepository.java
@@ -14,7 +14,7 @@ public interface GastoRepository extends CrudRepository<Gasto, Integer>{
 
 	
 	
-	@Query(value = "SELECT g.cantidad, g.fecha, prov.nombre as nombreProveedor, res.nombre , p.proveedor_id as idProveedor, r.restaurante_id as idRestaurante, g.id_gasto as idGasto FROM gasto g join proveedor_gastos p on g.id_gasto = p.gastos_id_gasto join proveedor prov on p.proveedor_id = prov.id join restaurante_gastos r on g.id_gasto = r.gastos_id_gasto join restaurante res on res.id = r.restaurante_id", nativeQuery = true)    
+	@Query(value = "SELECT g.cantidad, g.fecha, prov.nombre as nombreProveedor, res.nombre , p.proveedor_id as idProveedor, r.restaurante_id as idRestaurante, g.id_gasto as idGasto FROM gasto g join proveedor_gastos p on g.id_gasto = p.gastos_id_gasto join proveedor prov on p.proveedor_id = prov.id join restaurante_gastos r on g.id_gasto = r.gastos_id_gasto join restaurante res on res.id = r.restaurante_id ORDER BY fecha DESC", nativeQuery = true)    
 	public List<RelGastos> getRestauranteGasto();
 	
 	
--- src/main/java/com/app/fct/repositories/IngresoRepository.java
+++ src/main/java/com/app/fct/repositories/IngresoRepository.java
@@ -11,7 +11,7 @@ import com.app.fct.models.RelIngreso;
 
 public interface IngresoRepository extends CrudRepository<Ingreso, Integer>{
 	
-	@Query(value="SELECT i.id_ingreso as idIngreso, i.fecha, i.cantidad, i.fuente, res.nombre as nombreRestaurante , r.ingresos_id_ingreso as idRestaurante FROM ingreso i join restaurante_ingresos r on r.ingresos_id_ingreso = i.id_ingreso join restaurante res on res.id=r.restaurante_id", nativeQuery=true )
+	@Query(value="SELECT i.id_ingreso as idIngreso, i.fecha, i.cantidad, i.fuente, res.nombre as nombreRestaurante , r.ingresos_id_ingreso as idRestaurante FROM ingreso i join restaurante_ingresos r on r.ingresos_id_ingreso = i.id_ingreso join restaurante res on res.id=r.restaurante_id ORDER BY fecha DESC", nativeQuery=true )
 	public List<RelIngreso> getRestauranteIngreso();
 	
 	
